514542ca8520bd0a804a860651038ad1afd990b8
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-07 21:27:56 +0200

    feat(project3): stage 1 finished

--- Project3/app/src/main/java/com/lavertis/project3/MainActivity.java
+++ Project3/app/src/main/java/com/lavertis/project3/MainActivity.java
@@ -1,14 +1,33 @@
 package com.lavertis.project3;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText urlEditText;
+    private Button downloadInfoButton;
+    private Button downloadFileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        urlEditText = findViewById(R.id.urlEditText);
+        downloadInfoButton = findViewById(R.id.downloadInfoButton);
+        downloadFileButton = findViewById(R.id.downloadFileButton);
+
+        downloadInfoButton.setOnClickListener(v -> {
+            String url = urlEditText.getText().toString();
+            if (url.isEmpty()) {
+                urlEditText.setError("URL is empty");
+            } else {
+                AsynchronousTask task = new AsynchronousTask(this);
+                task.execute(url);
+            }
+        });
     }
 }
